debf53daa994ac152b259618d3ae0589dd77586c
Zeynep Toy <zeynep@Hakan-MacBook-Pro.local>
2025-04-20 20:54:22 +0300

    add xcode project

---
+++ ilk bebegim/ilk bebegim/ilk_bebegimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilk_bebegimApp.swift
+//  ilk bebegim
+//
+//  Created by Zeynep Toy on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ilk_bebegimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
